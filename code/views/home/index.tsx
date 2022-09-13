00da4a7f9a11a879f4344e7d8596328ad2ffb31a
pickleSauce <anxinyang114@gmail.com>
2022-09-13 02:18:42 +0000

    Update index.tsx

--- code/views/home/index.tsx
+++ code/views/home/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import {FeedTime} from "@defines/global-types";
-import {styles} from "./index.module.css";
+import {STORAGE_KEY,MAX_DISPLAY_RECORD, VOL_LIST} from '@defines/global-const'
+import styles from "./index.module.css";
 
-const STORAGE_KEY = 'feedTimes';
 
 const Home: NextPage = () => {
   
   const [feedTimes, setFeedTimes] = useState<FeedTime[]>([]);
-  const latestFeedTime = feedTimes[feedTimes.length-1] ?? {};
+   
   const loadFeedTimes = ()=>{
     const json = localStorage.getItem(STORAGE_KEY) ?? "[]";
     const feedTimes = JSON.parse(json);
@@ -16,19 +16,53 @@ const Home: NextPage = () => {
     setFeedTimes(feedTimes);
   }
   
-  const updateFeedTimes = ()=>{
-    const json = JSON.stringify(feedTimes);
+  const updateFeedTimes = (vol?:number)=>{
+    const arr = [...feedTimes];
+    if(vol){
+      arr.unshift({
+        date:Date.now(),
+        volumn:vol
+      })
+    }
+    const json = JSON.stringify(arr);
     localStorage.setItem(STORAGE_KEY, json);
+    setFeedTimes(arr);
+  }
+  
+  const formatTime = (date?:number)=>{
+    if(!date)return '';
+    const d = new Date(date);
+    
+    return `${d.getHours()}:${d.getMinutes()}`;
+    
+  }
+  
+  const formatDiff = (date?:number)=>{
+    if(!date)return '';
+    const diff = Date.now() - date;
+    
+    return `${(diff/1000/60).toFixed(0)} mins ago`
   }
   
   return (
-    <div onLoad={loadFeedTimes}>
-      <section>
-          <h1>Last Feed:</h1>
-          </section>
-          <section>
-            <h1>{latestFeedTime.volumn?? "-"}oz at {latestFeedTime.date??"-"}</h1>
+    <div className={styles.container} onLoad={loadFeedTimes}>
+      <div className={styles.timesContainer}>
+          <section className={styles.timeList}>
+          {feedTimes.filter((f,i)=>i<MAX_DISPLAY_RECORD).map((ft,idx)=>{
+            return <div key={idx}><span style={{
+              fontSize:`${2-(0.2*idx)}rem`,
+              opacity:`${1 - ((1/MAX_DISPLAY_RECORD)* idx)}`
+            }}>{formatTime(ft.date)} - {ft.volumn??''}oz</span>
+            {idx ===0?<span>{formatDiff(ft.date)}</span>:null}
+            </div>
+          })}
             </section>
+            </div>
+            <section>
+              {VOL_LIST.map((v,i)=>{
+                return <button className={styles.volBtn} onClick={()=>updateFeedTimes(v)} key={i}>{v}oz</button>
+              })}
+              </section>
     </div>
   )
 }
